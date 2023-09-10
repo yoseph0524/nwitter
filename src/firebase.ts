9e5bcf5e8b3f9b9afb06d9e519282124f59b6203
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-09 18:24:46 -0700

    initialize firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCinOzAIkGDDQDAFaeE_-niQSBHGwHczeU",
+  authDomain: "nwitter-6bec6.firebaseapp.com",
+  projectId: "nwitter-6bec6",
+  storageBucket: "nwitter-6bec6.appspot.com",
+  messagingSenderId: "881432177546",
+  appId: "1:881432177546:web:809fbb8b3079dff8557673",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
